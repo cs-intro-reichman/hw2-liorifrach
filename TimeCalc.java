08175c7b90f328680cb2f4b3ca66318820f5a608
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-18 21:22:08 +0200

    time calc added

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,18 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+
+        String timeInput = args[0];
+        int minutesToadd = Integer.parseInt(args[1]);
+
+        int hours = Integer.parseInt(timeInput.substring(0, 2));
+        int minutes = Integer.parseInt((timeInput.substring(3, 5)));
+
+        int totlalminutes = (hours * 60) + minutes + minutesToadd;
+
+        int newhours = (totlalminutes / 60) % 24; 
+        int newminuts = totlalminutes % 60;
+
+        System.out.printf("%02d:%02d\n", newhours, newminuts);
+
     }
 }
