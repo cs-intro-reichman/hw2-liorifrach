e8e18fd036baef1c78a1ae59200a76fb6c441e9f
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-19 12:49:24 +0200

    cheers added

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,27 @@
-// Prints a crowd cheering output.
+
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+            String input = args[0].toUpperCase(); // נהפוך לאותיות גדולות
+            int count = Integer.parseInt(args[1]); // המרת המספר
+    
+            for (int i = 0; i < input.length(); i++) {
+                char ch = input.charAt(i);
+                if (isVowelOrSpecial(ch)) {
+                    System.out.println("Give me an " + ch + ": " + ch + "!");
+                } else {
+                    System.out.println("Give me a " + ch + ": " + ch + "!");
+                }
+            }
+    
+            for (int i = 0; i < count; i++) {
+                System.out.println(input + "!!!");
+            }
         }
-}
+    
+   
+        public static boolean isVowelOrSpecial(char ch) {
+            String specialLetters = "AEFHILMNORSX"; 
+            return specialLetters.indexOf(ch) >= 0; 
+        }
+    }
+    
