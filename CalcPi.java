360b6fe52abba4f1eaff80a40d9f6dfefa76fd3b
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-20 17:42:00 +0200

    caldpi added

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,19 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+
+		int number = Integer.parseInt(args[0]);
+		double sum = 0.0; 
+		for (int i=0; i<number; i++){
+			double term = 1.0 / (2 * i + 1); 
+			if(i%2 ==0){
+				sum += term; 
+			}
+			else{
+				sum -= term; 
+			}
+		}
+		System.out.println("pi accorging to java: " + Math.PI);
+		System.out.println("pi, approximated: " + (4 * sum));
 	}
 }
