86e354b2219ca35258704fe3cce591fa8bf50ed5
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-21 14:06:16 +0200

    pi added

--- CalcPi.java
+++ CalcPi.java
@@ -3,17 +3,17 @@ public class CalcPi {
 	public static void main(String [] args) { 
 
 		int number = Integer.parseInt(args[0]);
-		double sum = 0.0; 
+		double sum = 0; 
 
-		for (int i=0; i < number; i++){
-			double term = Math.pow(-1, i) / (2 * i +1);
+		for (int i = 0; i < number; i++){
+			double term = Math.pow(-1, i) / (2 * i + 1);
 			sum = sum + term;
 		}
 
 		sum = sum * 4;
 			
 		System.out.println("pi according to java: " + Math.PI);
-		System.out.println("pi, approximated:     " + String.format("%.15f", sum));
-
+		System.out.println("pi, approximated:     " + sum);
+	
 	}
 }
