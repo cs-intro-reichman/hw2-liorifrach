b713f2fe4791f3667d53596bb5a7806cdfee8af7
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-21 12:25:12 +0200

    new cheers

--- Cheers.java
+++ Cheers.java
@@ -1,27 +1,30 @@
-
 public class Cheers {
-        public static void main(String[] args) {
-            String input = args[0].toUpperCase(); // נהפוך לאותיות גדולות
-            int count = Integer.parseInt(args[1]); // המרת המספר
-    
-            for (int i = 0; i < input.length(); i++) {
-                char ch = input.charAt(i);
-                if (isVowelOrSpecial(ch)) {
-                    System.out.println("Give me an " + ch + ": " + ch + "!");
-                } else {
-                    System.out.println("Give me a " + ch + ": " + ch + "!");
-                }
-            }
-    
-            for (int i = 0; i < count; i++) {
-                System.out.println(input + "!!!");
+    public static void main(String[] args) {
+        String input = args[0].toUpperCase(); 
+        int count = Integer.parseInt(args[1]); 
+        
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (isVowelOrSpecial(ch)) {
+                System.out.println("Give me an " + ch + ": " + ch + "!");
+            } else {
+                System.out.println("Give me a " + ch + ": " + ch + "!");
             }
         }
-    
-   
-        public static boolean isVowelOrSpecial(char ch) {
-            String specialLetters = "AEFHILMNORSX"; 
-            return specialLetters.indexOf(ch) >= 0; 
+
+       
+        System.out.println("What does that spell?");
+        
+      
+        for (int i = 0; i < count; i++) {
+            System.out.println(input + "!!!");
         }
     }
-    
+
+  
+    public static boolean isVowelOrSpecial(char ch) {
+        String specialLetters = "AEFHILMNORSX"; 
+        return specialLetters.indexOf(ch) >= 0; 
+    }
+}
